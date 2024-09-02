b56bdbcdf5e80f1a2b307893b293a2731f0f5027
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-09-02 10:00:02 +0700

    create account resource for server purposes

--- app/Http/Controllers/Api/AccountApiController.php
+++ app/Http/Controllers/Api/AccountApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AccountResource;
 use App\Http\Resources\BlogResource;
 
 class AccountApiController extends Controller
@@ -14,7 +15,7 @@ class AccountApiController extends Controller
         $account = User::get();
 
         if ($account->count() > 0) {
-            return BlogResource::collection($account);
+            return AccountResource::collection($account);
         } else {
             return response()->json([
                 'message' => 'Tidak ada data '
